2853a68c0ab2f71c32188e76e9c3a0562dfca6ed
Ryota Ozaki <ozaki.ryota@gmail.com>
2020-09-13 21:27:37 +0000

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+for file in .gitconfig .tmux.conf .vimrc .zshrc; do
+	target=$HOME/$file
+	if [ -f $target ]; then
+		echo "warning: $target already exists"
+	else
+		cp $file $target
+	fi
+done
+for dir in .tmux; do
+	target=$HOME/$dir
+	if [ -d $target ]; then
+		echo "warning: $target already exists"
+	else
+		cp -r $dir $HOME
+	fi
+done
+
+mkdir -p ~/.vim/colors
+curl https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim > ~/.vim/colors/atom-dark-256.vim
