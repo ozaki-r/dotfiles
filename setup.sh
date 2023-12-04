4b25510a7559a6e97378f748163efb028a4012e8
Ryota Ozaki <ozaki.ryota@gmail.com>
2023-12-04 14:12:10 +0900

    fish: add a config script for prompt

--- setup.sh
+++ setup.sh
@@ -17,6 +17,16 @@ for dir in .tmux; do
 	fi
 done
 
+# fish
+mkdir -p $HOME/.config/fish/functions/
+file=".config/fish/functions/fish_prompt.fish"
+target=$HOME/$file
+if [ -f $target ]; then
+	echo "warning: $target already exists"
+else
+	cp $file $target
+fi
+
 mkdir -p ~/bin
 
 mkdir -p ~/.vim/colors
